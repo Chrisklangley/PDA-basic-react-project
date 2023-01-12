fbdc6c6dd5f08bc46c22534c077b2cc51e242a4e
Christopher langley <christopherlangley@Christophers-MacBook-Pro.local>
2023-01-12 13:07:36 -0800

    PDA-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import ProfileCard from "./PofileCard";
+import AlexaImg from "./images/alexa.png";
+import CortanaImg from "./images/cortana.png";
+import SiriImg from "./images/siri.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Pesonal digital Assistants</p>
+        </div>
+      </section>
+      <div> </div>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImg}
+                content="Alexa wss created by amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana99"
+                image={CortanaImg}
+                content="Cortana was made my microsoft idk wtf it does"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImg}
+                content="made by Apple is always listening"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
